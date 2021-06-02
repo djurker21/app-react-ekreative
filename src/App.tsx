75e355f019c7558d77b1de79fa2dacd7976d5c5d
Yurii Vydai <yurii.vydai@ekreative.com>
2021-06-02 17:44:56 +0300

    google login, facebook login

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+
+const responseGoogle = (response: any) => {
+  console.log(response);
+}
+
+const responseFacebook = (response: any) => {
+  console.log(response);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <GoogleLogin
+        clientId={`${process.env.REACT_APP_GOOGLE_ID}`}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      <br />
+      <FacebookLogin
+        appId={`${process.env.REACT_APP_FB_ID}`}
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
     </div>
   );
 }
