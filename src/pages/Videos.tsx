d4888f9d43e59f9149300495cc4285c42af1cc4b
Yurii Vydai <yurii.vydai@ekreative.com>
2021-06-07 12:49:56 +0300

    youtube videos, rxjs

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,63 +1,49 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Video, YoutubeResult } from "../app/types";
+import { youtubeService } from "../services/youtube.service";
 
 export function Videos() {
-  const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
-  const channelId = process.env.REACT_APP_YOUTUBE_CHANNEL;
-  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=${channelId}&key=${apiKey}`;
-
   const defaultYoutubeResult: YoutubeResult = { items: [] };
 
   const [youtubeResult, setYoutubeResult]: [
     YoutubeResult,
     (youtubeResult: YoutubeResult) => void
   ] = useState(defaultYoutubeResult);
-  const [loading, setLoading]: [boolean, (loading: boolean) => void] =
-    useState<boolean>(true);
-  const [error, setError]: [string, (error: string) => void] = useState("");
 
   useEffect(() => {
-    axios
-      .get<YoutubeResult>(url, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        setYoutubeResult(response.data);
-        setLoading(false);
-      })
-      .catch((ex) => {
-        const error =
-          ex.response.status === 404
-            ? "Resource Not found"
-            : "An unexpected error has occurred";
-        setError(error);
-        setLoading(false);
-      });
-  });
+    youtubeService.getChannelVideos();
+    const subscription = youtubeService.channelVideosData.subscribe(
+      (data: YoutubeResult) => {
+        setYoutubeResult(data);
+      }
+    );
+    return subscription.unsubscribe;
+  }, []);
 
-  if (loading) {
-    return <div>loading...</div>;
+  if (youtubeResult) {
+    return (
+      <div className="App">
+        <ul className="videos">
+          {youtubeResult.items.map((video: Video) => (
+            <li key={video.id.videoId}>
+              <img
+                src={video.snippet.thumbnails.default.url}
+                alt={"video_thumbnail"}
+              />
+              <p>{video.snippet.title}</p>
+              <a href={"https://localhost:3000/video/" + video.id.videoId}>
+                open
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>loading...</p>
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      <ul className="posts">
-        {youtubeResult.items.map((video: Video) => (
-          <li key={video.id.videoId}>
-            <img
-              src={video.snippet.thumbnails.default.url}
-              alt={"video_thumbnail"}
-            />
-            <p>{video.snippet.title}</p>
-            <a href={"https://localhost:3000/video/" + video.id.videoId}>
-              open
-            </a>
-          </li>
-        ))}
-      </ul>
-      {error && <p className="error">{error}</p>}
-    </div>
-  );
 }
--- src/services/youtube.service.ts
+++ src/services/youtube.service.ts
@@ -3,18 +3,24 @@ import axios from "axios";
 import { BehaviorSubject } from "rxjs";
 
 const videoSubject = new BehaviorSubject(null);
+const channelVideosSubject = new BehaviorSubject(null);
 
 const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+const channelId = process.env.REACT_APP_YOUTUBE_CHANNEL;
 
 export const youtubeService = {
   getVideoInfo,
   videoData: videoSubject.asObservable(),
-  get videoDataValue() {
-    return videoSubject.value;
-  },
+  getChannelVideos,
+  channelVideosData: channelVideosSubject.asObservable(),
 };
 
 function getVideoInfo(videoId: string) {
   const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${apiKey}`;
+  axios.get(url).then((res) => videoSubject.next(res.data));
+}
+
+function getChannelVideos() {
+  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=${channelId}&key=${apiKey}`;
   axios.get(url).then((res) => videoSubject.next(res.data));
 }
